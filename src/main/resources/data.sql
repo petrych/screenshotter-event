e5346cf6cc21be92ae6c86139a6cae7fddef7d9c
Anastasiia Petrych <a.petrych@gmail.com>
2020-05-09 09:54:29 +0200

    Added ".png" extension screenshot filenames

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO Screenshot(id, name, date_created) VALUES (1, 'google-com', '2020-05-01');
-INSERT INTO Screenshot(id, name, date_created) VALUES (2, 'stackoverflow-com', '2020-05-02');
+INSERT INTO Screenshot(id, name, date_created) VALUES (1, 'google-com.png', '2020-05-01');
+INSERT INTO Screenshot(id, name, date_created) VALUES (2, 'stackoverflow-com.png', '2020-05-02');
