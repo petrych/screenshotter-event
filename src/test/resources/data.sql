7bc82b7431d98f45d90dd32a0b5b3228e8a82d24
Anastasiia Petrych <a.petrych@gmail.com>
2021-03-23 14:30:19 +0100

    In the DB, the URI of a screenshot now contains its id instead of its name

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,3 @@
-insert into Screenshot(id, name, uri, date_created) values (4, 'screenshot-1-com.png', 'http://localhost:8083/screenshotter/screenshots/screenshot-1-com.png', '2020-05-04');
-insert into Screenshot(id, name, uri, date_created) values (5, 'screenshot-2-com.png', 'http://localhost:8083/screenshotter/screenshots/screenshot-2-com.png', '2020-05-05');
-insert into Screenshot(id, name, uri, date_created) values (6, 'picture-com.png', 'http://localhost:8083/screenshotter/screenshots/picture-com.png', '2020-05-06');
+insert into Screenshot(id, name, uri, date_created) values (4, 'screenshot-1-com.png', 'http://localhost:8083/screenshotter/screenshots/4', '2020-05-04');
+insert into Screenshot(id, name, uri, date_created) values (5, 'screenshot-2-com.png', 'http://localhost:8083/screenshotter/screenshots/5', '2020-05-05');
+insert into Screenshot(id, name, uri, date_created) values (6, 'picture-com.png', 'http://localhost:8083/screenshotter/screenshots/6', '2020-05-06');
