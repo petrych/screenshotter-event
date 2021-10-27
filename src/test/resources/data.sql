880eac4240d6f0d49ac1804a99d2042b52e7b10f
Anastasiia Petrych <a.petrych@gmail.com>
2021-10-27 15:56:19 +0200

    Updated database creation and population for test environment

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,12 @@
-insert into Screenshot(id, name, uri, date_time_created) values (4, 'screenshot-1-com.png', 'http://localhost:8083/screenshotter/screenshots/4', '2020-05-04');
-insert into Screenshot(id, name, uri, date_time_created) values (5, 'screenshot-2-com.png', 'http://localhost:8083/screenshotter/screenshots/5', '2020-05-05');
-insert into Screenshot(id, name, uri, date_time_created) values (6, 'drive-google-com.png', 'http://localhost:8083/screenshotter/screenshots/6', '2020-05-06');
+DROP TABLE IF EXISTS SCREENSHOT;
+
+CREATE TABLE SCREENSHOT (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(250) NOT NULL,
+  uri VARCHAR(250) NOT NULL,
+  date_time_created VARCHAR(250) NOT NULL
+);
+
+INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (4, 'screenshot-1-com.png', 'http://localhost:8080/screenshotter/screenshots/4', '2020-05-04');
+INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (5, 'screenshot-2-com.png', 'http://localhost:8080/screenshotter/screenshots/5', '2020-05-05');
+INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (6, 'drive-google-com.png', 'http://localhost:8080/screenshotter/screenshots/6', '2020-05-06');
