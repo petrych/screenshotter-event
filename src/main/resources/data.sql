131163434d29c127e1c4621b715af8c92d9927e1
Anastasiia Petrych <a.petrych@gmail.com>
2021-10-27 15:55:53 +0200

    Updated database creation and population for production

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
-INSERT INTO Screenshot(id, name, uri, date_time_created) VALUES (1, 'google-com.png', 'http://localhost:8083/screenshotter/screenshots/1', '2020-05-01');
-INSERT INTO Screenshot(id, name, uri, date_time_created) VALUES (2, 'stackoverflow-com.png', 'http://localhost:8083/screenshotter/screenshots/2', '2020-05-02');
-INSERT INTO Screenshot(id, name, uri, date_time_created) VALUES (3, 'google-2-com.png', 'http://localhost:8083/screenshotter/screenshots/3', '2020-05-14');
+DROP TABLE IF EXISTS SCREENSHOT;
+
+CREATE TABLE SCREENSHOT (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(250) NOT NULL,
+  uri VARCHAR(250) NOT NULL,
+  date_time_created VARCHAR(250) NOT NULL
+);
+
+INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (1, 'google-com.png', 'http://localhost:8083/screenshotter/screenshots/1', '2020-05-01');
+INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (2, 'stackoverflow-com.png', 'http://localhost:8083/screenshotter/screenshots/2', '2020-05-02');
+INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (3, 'google-2-com.png', 'http://localhost:8083/screenshotter/screenshots/3', '2020-05-14');
