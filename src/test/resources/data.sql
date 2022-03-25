9d6acc5d9c57ccd0cc58f861a9a08afa53329892
Anastasiia Petrych <a.petrych@gmail.com>
2022-03-25 10:37:53 +0100

    Resolves #28 "Configure a database for testing"

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,12 +1,10 @@
-DROP TABLE IF EXISTS SCREENSHOT;
-
-CREATE TABLE SCREENSHOT (
+CREATE TABLE IF NOT EXISTS screenshot (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(250) NOT NULL,
   uri VARCHAR(250) NOT NULL,
   date_time_created VARCHAR(250) NOT NULL
 );
 
-INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (4, 'screenshot-1-com.png', 'http://localhost:8080/screenshotter/screenshots/4', '2020-05-04');
-INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (5, 'screenshot-2-com.png', 'http://localhost:8080/screenshotter/screenshots/5', '2020-05-05');
-INSERT INTO SCREENSHOT (id, name, uri, date_time_created) VALUES (6, 'drive-google-com.png', 'http://localhost:8080/screenshotter/screenshots/6', '2020-05-06');
+INSERT IGNORE INTO screenshot (id, name, uri, date_time_created) VALUES (1, 'google-com.png', 'http://localhost:8080/screenshotter/screenshots/1', '1999-05-01');
+INSERT IGNORE INTO screenshot (id, name, uri, date_time_created) VALUES (2, 'stackoverflow-com.png', 'http://localhost:8080/screenshotter/screenshots/2', '1999-05-02');
+INSERT IGNORE INTO screenshot (id, name, uri, date_time_created) VALUES (3, 'google-2-com.png', 'http://localhost:8080/screenshotter/screenshots/3', '1999-05-14');
