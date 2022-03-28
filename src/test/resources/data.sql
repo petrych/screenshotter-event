346a279d4db93e7e32022886d34f28458e949929
Anastasiia Petrych <a.petrych@gmail.com>
2022-03-28 09:34:31 +0200

    Resolves #28 "Configure a database for testing". Added sql script execution to the integration test

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS screenshot (
   date_time_created VARCHAR(250) NOT NULL
 );
 
-INSERT IGNORE INTO screenshot (id, name, uri, date_time_created) VALUES (1, 'google-com.png', 'http://localhost:8080/screenshotter/screenshots/1', '1999-05-01');
-INSERT IGNORE INTO screenshot (id, name, uri, date_time_created) VALUES (2, 'stackoverflow-com.png', 'http://localhost:8080/screenshotter/screenshots/2', '1999-05-02');
-INSERT IGNORE INTO screenshot (id, name, uri, date_time_created) VALUES (3, 'google-2-com.png', 'http://localhost:8080/screenshotter/screenshots/3', '1999-05-14');
+INSERT IGNORE INTO screenshot (name, uri, date_time_created) VALUES ('screenshot-1-com.png', 'http://localhost:8080/screenshotter/screenshots/1', '1999-05-01');
+INSERT IGNORE INTO screenshot (name, uri, date_time_created) VALUES ('screenshot-2-com.png', 'http://localhost:8080/screenshotter/screenshots/2', '1999-05-02');
+INSERT IGNORE INTO screenshot (name, uri, date_time_created) VALUES ('drive-google-com.png', 'http://localhost:8080/screenshotter/screenshots/3', '1999-05-14');
